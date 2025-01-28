a9496f5d322f1c1aa1004889dc73e6df2a082fc4
vanesa-trejo <vanesainformatica41@gmail.com>
2025-01-28 13:46:42 -0600

    Add files via upload
    se agrego la carpeta de javascript

---
+++ JAVASCRIPT/script.js
@@ -0,0 +1,6 @@
+//obtener el boton por su ID que se puse
+const boton=document.getElementById('botoncito');
+// Agregar un evento al boton
+boton.addEventListener('click',function(){
+    alert("hola ya le diste click");
+});
